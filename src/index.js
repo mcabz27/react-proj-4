a1b884340066efee2196b423dfddf42ee221ca3a
Matthew Cabezon <matt.cabezon@gmail.com>
2017-01-18 12:26:38 -0500

    fix ind js

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
 import React from "react";
 
 import ReactDOM from "react-dom";
-import App from './app/App';
-// const Root = (
-//   window.ReactGoogleMapsAsync ?
-//   require(`./app/AsyncApp`).default :
+// import App from './app/App';
+const Root = (
+  // window.ReactGoogleMapsAsync ?
+  // require(`./app/AsyncApp`).default :
 
-//    * If you're not using async,
-//    *
-//    * Add script src="https://maps.googleapis.com/maps/api/js" to your HTML to provide google.maps reference
+  //  * If you're not using async,
+  //  *
+  //  * Add script src="https://maps.googleapis.com/maps/api/js" to your HTML to provide google.maps reference
 
-//   require(`./app/App`).default
-// );
+  require(`./app/App`).default
+);
 
 import "./index.css";
 
 ReactDOM.render(
-  <App />,
+  <Root />,
   document.getElementById(`root`)
 );
